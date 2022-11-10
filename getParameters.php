60153f81e8b4a7b339ddba20cf258ddafc924c1a
FilipSajna <114649863+FilipSajna@users.noreply.github.com>
2022-11-10 09:24:14 +0100

    update2

--- getParameters.php
+++ getParameters.php
@@ -1,6 +1,6 @@
 <?php
         echo '<h1>getParameters page</h1>';
-        echo '<h2>https://espwebboss.azurewebsites.net</h1>';
+        echo '<h2>https://espwebboss.azurewebsites.net/getParameters.php?a=10&b=2</h1>';
         
         $sn1 = $_GET["a"];
         $sn2 = $_GET["b"];
