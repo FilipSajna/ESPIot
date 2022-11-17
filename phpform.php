7cc677c55c8f61788d9ab6480ea56db534f46526
FilipSajna <114649863+FilipSajna@users.noreply.github.com>
2022-11-17 14:43:29 +0100

    inputs

--- phpform.php
+++ phpform.php
@@ -2,9 +2,16 @@
 <body>
 
 <form action="phpform.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
+Name :      <input type="text" name="name required"><br>
+Surname :   <input type="text" name="surname" required><br>
+E-mail :    <input type="email" name="email" required><br>
+Adress :    <input type="text" name="adress"required><br>
+Number :    <input type="number" name="number" required><br>
+Postal code : <input type="number" name="psc" required><br>
+State :     <input type="text" name="state" required><br>
+Phone number : <input type="tel" name="phonenumber" pattern="^\d{4}\d{3}\d{3}$" placeholder="Napr. 0940 093 090" required><br>
 <input type="submit">
+<input type="reset">
 </form>
 
 </body>
