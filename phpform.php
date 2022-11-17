848ec55c81dc95e1947d02f07e1486b3d6c1a67f
FilipSajna <114649863+FilipSajna@users.noreply.github.com>
2022-11-17 14:35:16 +0100

    nvm

--- phpform.php
+++ phpform.php
@@ -1,4 +1,11 @@
-<?php
-    echo '<h1>Form page</h1>';
-    Name: <input type="text" name="name">
-?>
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
