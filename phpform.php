d4c804550165179ccb6fde15bea76517d6c8cdba
FilipSajna <114649863+FilipSajna@users.noreply.github.com>
2022-11-17 20:46:53 +0100

    label sizes + bcg

--- phpform.php
+++ phpform.php
@@ -7,59 +7,69 @@
 }
 
 body {
-  font-family: Arial, Helvetica, sans-serif;
+    background-image: url("content/bck1.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: center;
+    font-family: Arial, Helvetica, sans-serif;
 }
 
 .header {
-  grid-area: header;
-  background-color: #f1f1f1;
-  padding: 30px;
-  text-align: center;
-  font-size: 35px;
+    grid-area: header;
+    background-color: #f1f1f1;
+    padding: 30px;
+    text-align: center;
+    font-size: 35px;
 }
 
 .grid-container {
-  display: grid;
-  grid-template-areas: 
-    'header header header header header header' 
-    'left middle middle middle middle right' 
-    'footer footer footer footer footer footer';
+    display: grid;
+    grid-template-areas: 
+        'header header header header header header' 
+        'left middle middle middle middle right' 
+        'footer footer footer footer footer footer';
 } 
 
 .left,
 .middle,
 .right {
-  padding: 10px;
-  height: 300px;
+    padding: 10px;
+    height: 300px;
 }
 
 .left {
-  grid-area: left;
+    grid-area: left;
 }
 
 .middle {
-  grid-area: middle;
+    grid-area: middle;
 }
 
 .right {
-  grid-area: right;
+    grid-area: right;
 }
 
 .footer {
-  grid-area: footer;
-  background-color: #f1f1f1;
-  padding: 10px;
-  text-align: center;
+    grid-area: footer;
+    background-color: #f1f1f1;
+    padding: 10px;
+    text-align: center;
+}
+
+.label {
+    font-size : 25px;
+    font-family : Arial;
 }
 
 @media (max-width: 600px) {
-  .grid-container  {
-    grid-template-areas: 
-      'header header header header header header' 
-      'left left left left left left' 
-      'middle middle middle middle middle middle' 
-      'right right right right right right' 
-      'footer footer footer footer footer footer';
+    .grid-container  {
+        grid-template-areas: 
+        'header header header header header header' 
+        'left left left left left left' 
+        'middle middle middle middle middle middle' 
+        'right right right right right right' 
+        'footer footer footer footer footer footer';
   }
 }
 </style>
@@ -69,16 +79,16 @@ body {
 <header><div class = "header"><h2 align = "center">Atipatapati</h2></div></header>
 
 <form action="phpform.php" method="post" style="text-align: center; ">
-Name :      <input type="text" name="name" required><br>
-Surname :   <input type="text" name="surname" required><br>
-E-mail :    <input type="email" name="email" required><br>
-Adress :    <input type="text" name="adress"required><br>
-Number :    <input type="number" name="number" required><br>
-Postal code : <input type="number" name="psc" required><br>
-State :     <input type="text" name="state" required><br>
-Phone number : <input type="tel" name="phonenumber" pattern="^\d{4}\d{3}\d{3}$" placeholder="Napr. 0940 093 090" required><br>
-<input type="submit">
-<input type="reset">
+<label for = "name" class = "label">Name : </label>      <input type="text" name="name" required><br>
+<label for = "surname" class = "label">Surname : </label>   <input type="text" name="surname" required><br>
+<label for = "email" class = "label">E-mail : </label>    <input type="email" name="email" required><br>
+<label for = "adress" class = "label">Adress : </label>    <input type="text" name="adress"required><br>
+<label for = "number" class = "label">Number : </label>    <input type="number" name="number" required><br>
+<label for = "psc" class = "label">Postal code : </label> <input type="number" name="psc" required><br>
+<label for = "state" class = "label">State : </label>     <input type="text" name="state" required><br>
+<label for = "phonenumber" class = "label">Phone number : </label> <input type="tel" name="phonenumber" pattern="^\d{4}\d{3}\d{3}$" placeholder="Napr. 0940 093 090" required><br>
+<input type="submit" class = "label">
+<input type="reset" class = "label">
 </form>
 
 </body>
