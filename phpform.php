1c859d830ebe4850756a194b405def5f16e552cf
FilipSajna <114649863+FilipSajna@users.noreply.github.com>
2022-11-18 16:07:55 +0100

    d

--- phpform.php
+++ phpform.php
@@ -1,6 +1,9 @@
 <html lang = "en">
 
-<head><title>Contact form</title><head>
+<head>
+    <meta charset="UTF-8">
+    <title>Contact form</title>
+    <head>
 <style>
 * {
     box-sizing: border-box;
@@ -101,14 +104,14 @@ label {
 
 <div class = "middle">
 <form method="post" style="text-align: center;">
-<label for = "name" class = "label">Name : </label>      <input type="text" name="name" required><br>
-<label for = "surname" class = "label">Surname : </label>   <input type="text" name="surname" required><br>
-<label for = "email" class = "label">E-mail : </label>    <input type="email" name="email" required><br>
-<label for = "adress" class = "label">Adress : </label>    <input type="text" name="adress"required><br>
-<label for = "number" class = "label">Number : </label>    <input type="number" name="number" required><br>
-<label for = "psc" class = "label">Postal code : </label> <input type="number" name="psc" required><br>
-<label for = "state" class = "label">State : </label>     <input type="text" name="state" required><br>
-<label for = "phonenumber" class = "label">Phone number : </label> <input type="tel" name="phonenumber" pattern="^\d{4}\d{3}\d{3}$" placeholder="E.g : 0940 093 090" required><br>
+<label class = "label">Name : </label>      <input type="text" name="name" required><br>
+<label class = "label">Surname : </label>   <input type="text" name="surname" required><br>
+<label class = "label">E-mail : </label>    <input type="email" name="email" required><br>
+<label class = "label">Adress : </label>    <input type="text" name="adress"required><br>
+<label class = "label">Number : </label>    <input type="number" name="number" required><br>
+<label class = "label">Postal code : </label> <input type="number" name="psc" required><br>
+<label class = "label">State : </label>     <input type="text" name="state" required><br>
+<label class = "label">Phone number : </label> <input type="tel" name="phonenumber" pattern="^\d{4}\d{3}\d{3}$" placeholder="E.g : 0940 093 090" required><br>
 <input type="submit" class = "label">
 <input type="reset" class = "label">
 </form>
@@ -130,59 +133,4 @@ fwrite($fp, $names);
 fclose($fp);
 }
 
-if(isset($_POST['surname']))
-{
-$surnames=$_POST['surname'];
-$fp = fopen('txtfiles/Surnames.txt', 'a');
-fwrite($fp, $surnames);
-fclose($fp);
-}
-
-if(isset($_POST['email']))
-{
-$emails=$_POST['email'];
-$fp = fopen('txtfiles/emails.txt', 'a');
-fwrite($fp, $emails);
-fclose($fp);
-}
-
-if(isset($_POST['adress']))
-{
-$adresses=$_POST['adress'];
-$fp = fopen('txtfiles/adresses.txt', 'a');
-fwrite($fp, $adresses);
-fclose($fp);
-}
-
-if(isset($_POST['number']))
-{
-$AdressNumbers=$_POST['number'];
-$fp = fopen('txtfiles/AdressNumbers.txt', 'a');
-fwrite($fp, $AdressNumbers);
-fclose($fp);
-}
-
-if(isset($_POST['psc']))
-{
-$pscs=$_POST['psc'];
-$fp = fopen('txtfiles/pscs.txt', 'a');
-fwrite($fp, $pscs);
-fclose($fp);
-}
-
-if(isset($_POST['state']))
-{
-$states=$_POST['state'];
-$fp = fopen('txtfiles/states.txt', 'a');
-fwrite($fp, $states);
-fclose($fp);
-}
-
-if(isset($_POST['phonenumber']))
-{
-$phonenumbers=$_POST['phonenumber'];
-$fp = fopen('txtfiles/phonenumbers.txt', 'a');
-fwrite($fp, $phonenumbers);
-fclose($fp);
-}
 ?>
