b89fc9d34945ecb151f1b5786ef65245ea763b3d
FilipSajna <114649863+FilipSajna@users.noreply.github.com>
2022-12-22 03:29:29 +0100

    push

---
+++ zadanie.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <title>Measured Air Quality</title>
+    <style>
+        html {
+            font-family: Helvetica;
+            display: inline-block;
+            margin: 0px auto;
+            text-align: center;
+        }
+
+        body {
+            margin-top: 50px;
+        }
+
+        h1 {
+            color: #444444;
+            margin: 50px auto 30px;
+        }
+
+        p {
+            font-size: 24px;
+            color: #444444;
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+
+
+<body>
+    <div id="webpage">
+        <h1>Measured Air Quality</h1>
+
+        <?php
+        echo '<h1>getParameters page</h1>';
+
+        $temp = $_GET["temp"];
+        $hum = $_GET["hum"];
+        $co2 = $_GET["co2"];
+        $tvoc = $_GET["tvoc"];
+
+        $text = "Temperature : " . $temp ;
+        $text2 = "Humidity  : " . $hum;
+        $text3 = "CO2 : " . $co2;
+        $text4 = " TVOC : " . $tvoc;
+
+        echo $text;
+?>
+
+    </div>
+</body>
+
+
+</html>
